ddaacc4ef5ac317d116467ea50a7a47cdf51136b
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2026-09-17 16:35:11 -0300

    Trabalhando com Strings

---
+++ Projetos/Proj04/Proj04.asm
@@ -0,0 +1,70 @@
+;----------------------------------------------------
+; Proj03.asm - Manipula strings
+; para compilar:
+; $ NMAKE
+; $ exe2bin proj04 proj04.com
+; Autor: Eng. Fabrício Ribeiro
+; Status: Não Concluído!
+;----------------------------------------------------
+
+CGROUP  GROUP   CODE_SEG, DATA_SEG
+        ASSUME  CS:CGROUP, DS:CGROUP
+
+CODE_SEG	SEGMENT PUBLIC
+
+        INCLUDE CONST.INC
+
+        ORG     100h
+
+	;Carrega funções externas
+        EXTRN   STR_PRINT:NEAR
+        EXTRN   STR_LEN:NEAR
+        EXTRN   STR_CMP:NEAR
+
+;...........................
+; Programa principal
+;...........................
+MAIN	PROC NEAR
+
+        CALL    STR_CMP
+        MOV     AL,STR_COMPARE
+        CMP     AL,TRUE
+        JE      IGUAIS
+        LEA     DX,MSG2
+        CALL    STR_PRINT
+        JMP     SAI_DOS
+IGUAIS: LEA     DX,MSG1
+        CALL    STR_PRINT
+
+SAI_DOS:
+	MOV     AH,4CH                  ;Retorna ao
+	INT     21H                     ;MS-DOS
+
+MAIN 	ENDP
+
+CODE_SEG	ENDS
+
+;****************************************************************
+; ÁREA DE DADOS
+;****************************************************************
+
+        PUBLIC  TEXTO, LEN, STRING1, STRING2
+
+DATA_SEG       SEGMENT PUBLIC
+
+        ;Texto para inserir:
+        TEXTO DB 'Esse eh o texto inserido no arquivo!',CR,LF,'$'
+        LEN DW ?
+        EXTERN STR_LENGHT:WORD                   ;tamanho do texto
+
+        ;Strings
+        STRING1 DB 'Essa eh a string 1!',CR,LF,'$'
+        STRING2 DB 'Essa eh a string 1!',CR,LF,'$'
+        EXTERN STR_COMPARE:BYTE
+
+        MSG1 DB 'As strings saum iguais!',CR,LF,'$'
+        MSG2 DB 'As strings saum diferentes!',CR,LF,'$'
+
+DATA_SEG       ENDS
+
+        END     MAIN
---
+++ Projetos/Proj04/strings.asm
@@ -0,0 +1,174 @@
+;****************************************************************
+; Essas rotinas manipulam strings
+;
+; CAR_PRINT - Imprime um caracter na tela;
+; STR_PRINT - Imprime uma string na tela;
+; STR_LEN - Calcula o número de bytes de uma string;
+;****************************************************************
+
+CGROUP          GROUP   CODE_SEG, DATA_SEG
+                ASSUME  CS:CGROUP, DS:CGROUP
+
+CODE_SEG        SEGMENT PUBLIC
+
+                INCLUDE CONST.INC
+
+;****************************************************************
+; CAR_PRINT
+; Esta rotina imprime uma caracter na tela
+; Entrada: DL - Código ASCII do caracter
+;****************************************************************
+
+                PUBLIC  CAR_PRINT
+
+CAR_PRINT       PROC    NEAR
+
+                PUSH    AX   
+                PUSH    DX
+
+IMPRESSO:       MOV     AH, 02H
+                INT     21h
+
+                POP     DX
+                POP     AX
+                RET
+
+CAR_PRINT       ENDP                
+
+;----------------------------------------------------------------
+; FIM CAR_PRINT
+;----------------------------------------------------------------
+
+;****************************************************************
+; STR_PRINT
+; Esta rotina imprime uma string na tela
+; Entrada: DX - Endereço base da string
+;****************************************************************
+
+                PUBLIC  STR_PRINT
+
+STR_PRINT       PROC    NEAR
+
+                PUSH    AX   
+                PUSH    DX
+
+                MOV     AH, 09H
+                INT     21h
+
+                POP     DX
+                POP     AX
+                RET
+
+STR_PRINT       ENDP
+
+;----------------------------------------------------------------
+; FIM FILE_CLOSE
+;----------------------------------------------------------------
+
+;****************************************************************
+; STR_LEN
+; Esta rotina calcula o número de bytes de uma string
+; Entrada: TEXTO - ponteiro da string
+; Saída:   STR_LENGHT - número de bytes da string
+;****************************************************************
+
+                PUBLIC  STR_LEN
+
+STR_LEN         PROC    NEAR
+
+                PUSH    AX
+                PUSH    CX
+                PUSH    SI
+                PUSH    DI
+
+                LEA     DI, TEXTO
+                MOV     SI, DI
+                MOV     CX, 0FFFFH
+                MOV     AL,'$'
+                CLD
+                REPNZ   SCASB
+                SUB     DI, SI
+                DEC     DI
+                MOV     STR_LENGHT, DI
+
+                POP     DI
+                POP     SI
+                POP     CX
+                POP     AX
+                RET
+
+STR_LEN         ENDP
+;----------------------------------------------------------------
+; FIM STR_LEN
+;----------------------------------------------------------------
+
+;****************************************************************
+; STR_CMP
+; Esta rotina compara 2 strings e colona na variável STR_COMPARE
+; o resultado da comparação.
+;
+; Entradas: STRING1 - ponteiro da string1
+;           STRING2 - ponteiro da string2
+; Saída:    STR_LENGHT - |0-diferentes|1-iguais|
+;****************************************************************
+
+                PUBLIC  STR_CMP
+
+STR_CMP         PROC    NEAR
+
+                PUSH    AX
+                PUSH    SI
+                PUSH    DI
+
+                LEA     SI, STRING1
+                LEA     DI, STRING2
+
+STR_CMP_INI:    MOV     AH, [SI]
+                MOV     AL, [DI]
+
+                CMP     AH, AL
+                JNE     STR_CMP_RET_FALSE
+                CMP     AH, '$'
+                JE      STR_CMP_RET_TRUE
+                INC     SI
+                INC     DI
+                JMP     STR_CMP_INI                
+
+STR_CMP_RET_TRUE:
+                MOV     AL, TRUE
+                MOV     STR_COMPARE, AL
+                JMP     STR_CMP_END
+
+STR_CMP_RET_FALSE:
+                MOV     AL, FALSE
+                MOV     STR_COMPARE, AL
+
+STR_CMP_END:    POP     DI
+                POP     SI
+                POP     AX
+                RET
+
+STR_CMP         ENDP
+;----------------------------------------------------------------
+; FIM STR_LEN
+;----------------------------------------------------------------
+
+CODE_SEG        ENDS
+
+;****************************************************************
+; ÁREA DE DADOS
+;****************************************************************
+                PUBLIC STR_LENGHT, STR_COMPARE
+
+DATA_SEG        SEGMENT PUBLIC
+
+                STR_LENGHT  DW  ?
+                STR_COMPARE  DB  ?
+                STRING1  DB  ?
+                STRING2  DB  ?
+                EXTERN TEXTO:BYTE   ;Recebe a variável externa
+                EXTERN CAR:BYTE     ;Recebe a variável externa
+                
+DATA_SEG        ENDS
+
+                END
